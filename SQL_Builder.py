32b7b0cb600a5df28c1bc3315fe22da06f689972
FXzoroark <48474446+FXzoroark@users.noreply.github.com>
2021-04-08 11:23:37 +0200

    undo et refonte du menu

--- SQL_Builder.py
+++ SQL_Builder.py
@@ -7,33 +7,15 @@ def replace_invalid_characters(name):
     return ''.join(e for e in name if e.isalnum())
 
 def res_do_it_for_all():
-    res = input("\n\x1b[32mDo it for all ? [y/N] \n\n\x1b[39m")
+    res = input("\nDo it for all ? [y/N] \n\n")
     if res.upper() == "Y":
-        print("\n\x1b[41mTHIS CHOICE WILL AFFECT ALL OTHER COLUMNS ON THIS LIST\x1b[49m\n\n")
+        print("\nTHIS CHOICE WILL AFFECT ALL OTHER COLUMNS ON THIS LIST\n\n")
         return True
     return False
 
-def yes_or_no():
-    while True:
-        answer = input("\n")
-        if answer.upper() == "Y":
-            return True
-        if answer.upper() == "N":
-            return False
-        else:
-            print("\nINVALID ENTRY")
-
-def confirm(query, con):
-    global confirmation
-    if confirmation == True:
-        print(query.as_string(con))
-        print("\nCONFIRM QUERY? (Y/N)")
-        if yes_or_no():
-            pass
-        else:
-            pass
-            ####################___________UNDO___________####################
-
+def print_listing(listing : dict):
+    for key in listing:
+        print(key + " - " + str(listing[key]))
 
 
 print("If you wish to make it faster, change the entries directly on the code.")
@@ -46,8 +28,6 @@ your_port = input("Insert Port: ")
 # This script will iterate with all CSV files in a folder. DO NOT leave any non CSV files in your folder.
 path = "C:/YourPath"   #Insert Path of your folder
 
-confirmation = True
-
 for file in os.listdir(path):
     if not file.endswith('.csv'):
         continue
@@ -58,69 +38,96 @@ for file in os.listdir(path):
     filepath = path + file
     f = open(filepath)
     line = f.readline()
-    listing = line.split(',')
 
-    types = {'1':"INT", '2':"BIGINT", '3':"VARCHAR", '4':"BOOL", '5':"SERIAL", '6':"DECIMAL", '7':"Change confirmation settings"}
-    diff = ["VARCHAR", "TURN OFF CONFIRMATION"]
+    dict_data = {replace_invalid_characters(column) : [] for column in line.split(',')}
+
+    if len(dict_data) == 0:
+        print("header not found => next file")
+        continue
+
+    data_names = list(dict_data.keys())
+    list_action = [[]]
+    types = {'1':"INT", '2':"BIGINT", '3':"VARCHAR", '4':"BOOL", '5':"SERIAL", '6':"DECIMAL"}
+    diff = ["VARCHAR"]
     do_it_for_all = False
-    data_type = 0
+    idx_column = 0
 
-    for column in listing:
-        while True:
-            if not do_it_for_all:
-                print(column)
-                data_type = int(input("\x1b[32mDataType? \n 1. INT \n 2. BIGINT \n 3. VARCHAR \n 4. BOOL \n 5. SERIAL \n 6. DECIMAL\x1b[39m\n\n"))
-            if data_type in types:
-                if types[data_type] not in diff:
-                    SQL_data.append(replace_invalid_characters(column) + " " + types[data_type] )
-                    if not do_it_for_all:
-                        do_it_for_all = res_do_it_for_all();
-                    break;
-                elif types[data_type] == "VARCHAR":
+
+
+
+    while True:
+            print("\nList column :\n")
+            print_listing(dict_data)
+            print("\nCurrent column :\n"+data_names[idx_column]+"\n")
+            res = input("\nMenu: (1) \n 1. next \n 2. do it for all \n 3. undo\n\n")
+            if res == '1' or res == '':
+                do_it_for_all = False
+            elif res == '2':
+                do_it_for_all = True
+            elif res == '3':
+                if(len(list_action) == 0):
+                    print("cannot execute undo")
+                    continue
+                for key in list_action[-1]:
+                    dict_data[key] = []
+                    idx_column -= 1
+                list_action.pop(-1)
+                continue
+            else:
+                continue
+
+            list_action.append([])
+            data_type = input("DataType? \n 1. INT \n 2. BIGINT \n 3. VARCHAR \n 4. BOOL \n 5. SERIAL \n 6. DECIMAL\n\n")
+            if data_type.isnumeric() and data_type in types:
+                dict_data[data_names[idx_column]].append(types[data_type])
+                if types[data_type] == "VARCHAR":
                     while True:
-                        if not do_it_for_all :
-                            max_size = input("\n\x1b[32mMax size of string?\x1b[39m\n\n")
+                        max_size = input("\nMax size of string?\n\n")
                         if max_size.isnumeric():
-                            SQL_data.append(replace_invalid_characters(column) + " VARCHAR(" + str(max_size) + ")")
-                            if not do_it_for_all:
-                                do_it_for_all = res_do_it_for_all();
+                            dict_data[data_names[idx_column]].append(max_size)
                             break;
                         else:
-                            print("\x1b[41mINVALID ENTRY\x1b[49m")
-                    break;
-                elif types[data_type] == "Change confirmation settings":
-                    if confirmation == True:
-                        print("\n Confirmation is set to True, would you like to change this setting?(Y/N)\nBy changing it, the program will no longer show you the query you built nor ask if you really wish to send it.")
-                        if yes_or_no():
-                            print("\n Confirmation was set to False, select 7 again to change this setting.")
-                            confirmation = False
-                        else:
-                            print("\n Confirmation settings will remain the same.")
-                    else:
-                        print("\n Confirmation is set to False, would you like to change this setting?(Y/N)\n By changing it, the program will start showing you the query and asking you if you confirm it's execution.")
-                        if yes_or_no():
-                            print("\n Confirmation was set to True, select 7 again to change this setting.")
-                            confirmation = True
-                        else:
-                            print("\n Confirmation settings will remain the same.")
+                            print("\nINVALID ENTRY\n")
+                list_action[-1].append(data_names[idx_column])
+                idx_column += 1
             else:
-                print('\x1b[41mINVALID ENTRY\x1b[49m')    
+                print('\nINVALID ENTRY\n')
+                continue    
+            
+            if do_it_for_all:
+                idx_tmp = idx_column-1
+                while idx_column != len(data_names):
+                    dict_data[data_names[idx_column]] = dict_data[data_names[idx_tmp]]
+                    list_action[-1].append(data_names[idx_column])
+                    idx_column += 1
+                    
+            
+            if idx_column == len(data_names):
+                res = input("\nAll defined (1):\n1. Finish\n2. Undo\n\n")
+                if res == '2':
+                    for key in list_action[-1]:
+                        dict_data[key] = []
+                        idx_column -= 1
+                    list_action.pop(-1)
+                else:
+                    break;
+            
+            
+
+
+    SQL_data = [f"{col} {dict_data[col][0]}" if len(dict_data[col]) == 1 else f"{col} {dict_data[col][0]}({dict_data[col][1]})" for col in dict_data]
 
     #After the iteration, the script will build a PostgreSQL query based on the information given
     string_SQL_data = ','.join(SQL_data)
     query = sql.SQL("CREATE TABLE {some_table} ({table_data});").format(
         some_table=sql.SQL(os.path.splitext(file)[0]),
         table_data=sql.SQL(string_SQL_data))
-    if confirmation == True:
-        confirm(query, con)
     cur.execute(query) 
     string_SQL_data = []
     con.commit()
     query = sql.SQL("COPY {some_table} FROM '{original_file}' DELIMITER ',' CSV HEADER ENCODING 'UTF8';").format(
         some_table=sql.SQL(os.path.splitext(file)[0]),
         original_file=sql.SQL(filepath))
-    if confirmation == True:
-        confirm(query, con)
     cur.execute(query) 
     con.commit()
     con.close()
