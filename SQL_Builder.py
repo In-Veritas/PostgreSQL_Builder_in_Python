2244d6ea914b3bc6fc2020d2b16ac25a884ee310
FXzoroark <48474446+FXzoroark@users.noreply.github.com>
2021-04-09 17:32:52 +0200

    export import sql et drop tables avec amélioration de l'interface

--- SQL_Builder.py
+++ SQL_Builder.py
@@ -1,5 +1,5 @@
 import psycopg2
-from psycopg2 import sql
+from psycopg2 import sql, errors
 import os
 import csv
 
@@ -13,6 +13,134 @@ def res_do_it_for_all():
         return True
     return False
 
+def create_from_csv(path_sql, path_csv, con, cur):
+    files = os.listdir(path_csv)
+    print(f"\nChoice the file ({len(files)+2}):")
+    for i in range(len(files)):
+        print(f"{i+1}. {files[i]}")
+    if len(files) >= 2:
+        print(f"{len(files)+1}. All of them")
+    print(f"{len(files)+2}. Cancel\n")
+    res = input()
+    selected_files = []
+    if not res.isnumeric() or int(res)<1 or int(res)>len(files)+1:
+        return
+    if len(files) >= 2 and int(res) == len(files)+1:
+        selected_files = files
+    else:
+        selected_files.append(files[int(res)-1])
+
+    for file in selected_files:
+        if not file.endswith('.csv'):
+            continue
+        print(f"file : {file}")
+        filepath = path_csv + file
+        f = open(filepath)
+        line = f.readline()
+
+        dict_data = {replace_invalid_characters(column) : [] for column in line.split(',')}
+        if len(dict_data) == 0:
+            print("header not found => next file")
+            continue
+
+        data_names = list(dict_data.keys())
+
+        list_action = [[]]
+        types = {'1':"INT", '2':"BIGINT", '3':"VARCHAR", '4':"BOOL", '5':"SERIAL", '6':"DECIMAL"}
+        diff = ["VARCHAR"]
+        do_it_for_all = False
+        idx_column = 0
+
+        while True:
+                print("\nList column :")
+                print_listing(dict_data)
+                print("\nCurrent column :\n"+data_names[idx_column])
+                res = input("\nMenu: (1) \n 1. next \n 2. do it for all \n 3. undo\n 4. suggest\n\n")
+                if res == '1' or res == '':
+                    do_it_for_all = False
+                elif res == '2':
+                    do_it_for_all = True
+                elif res == '3':
+                    if(len(list_action) == 0):
+                        print("cannot execute undo")
+                        continue
+                    for key in list_action[-1]:
+                        dict_data[key] = []
+                        idx_column -= 1
+                    list_action.pop(-1)
+                    continue
+                elif res == '4':
+                    dict_suggest = get_suggest_type(filepath, data_names)
+                    print("\nSuggest:")
+                    for key in dict_suggest:
+                        msg = f"{key} : "
+                        for type in dict_suggest[key]:
+                            msg += f"{type} "
+                        print(f"{msg}")
+                    continue
+                else:
+                    continue
+
+                list_action.append([])
+                data_type = input("\nDataType? \n 1. INT \n 2. BIGINT \n 3. VARCHAR \n 4. BOOL \n 5. SERIAL \n 6. DECIMAL\n\n")
+                if data_type.isnumeric() and data_type in types:
+                    dict_data[data_names[idx_column]].append(types[data_type])
+                    if types[data_type] == "VARCHAR":
+                        waiting_input("Max size of string", dict_data, data_names, idx_column)
+                    if types[data_type] == "DECIMAL":
+                        waiting_input("Total number size", dict_data, data_names, idx_column)
+                        waiting_input("Precision", dict_data, data_names, idx_column)
+                    list_action[-1].append(data_names[idx_column])
+                    idx_column += 1
+                else:
+                    print('\nINVALID ENTRY\n')
+                    continue
+                
+                if do_it_for_all:
+                    idx_tmp = idx_column-1
+                    while idx_column != len(data_names):
+                        dict_data[data_names[idx_column]] = dict_data[data_names[idx_tmp]]
+                        list_action[-1].append(data_names[idx_column])
+                        idx_column += 1
+                        
+                
+                if idx_column == len(data_names):
+                    res = input("\nAll defined (1):\n1. Finish\n2. Undo\n\n")
+                    if res == '2':
+                        for key in list_action[-1]:
+                            dict_data[key] = []
+                            idx_column -= 1
+                        list_action.pop(-1)
+                    else:
+                        break;
+        SQL_data = []
+        for col in dict_data:
+            if dict_data[col][0] == "VARCHAR":
+                SQL_data.append(f"{col} {dict_data[col][0]}({dict_data[col][1]})")
+            elif dict_data[col][0] == "DECIMAL":
+                SQL_data.append(f"{col} {dict_data[col][0]}({dict_data[col][1]}, {dict_data[col][2]})")
+            else:
+                SQL_data.append(f"{col} {dict_data[col][0]}")
+
+
+        #After the iteration, the script will build a PostgreSQL query based on the information given
+        string_SQL_data = ','.join(SQL_data)
+        table_name = file[:-4]
+
+        res = input("\nFinal step (1):\n1. Create table and import datas\n2. Create table without datas\n3. Export table without datas\n4. Export table with datas\n5. Cancel\n\n")
+        if res == "2":
+            execute_sql(sql.SQL(f"CREATE TABLE {table_name} ({string_SQL_data});"), con, cur)
+        elif res == "3":
+            export_sql(f"CREATE TABLE {table_name} ({string_SQL_data});\n", f"{path_sql}{table_name}.sql", "w")
+        elif res == "4":
+            export_sql(f"CREATE TABLE {table_name} ({string_SQL_data});\n", f"{path_sql}{table_name}.sql", "w")
+            export_sql(f"COPY {table_name} FROM '{path_csv}{table_name}.csv' DELIMITER ',' CSV HEADER ENCODING 'UTF8';\n", f"{path_sql}{table_name}.sql", "a")
+        elif res == "5":
+            continue
+        else:
+            execute_sql(f"CREATE TABLE {table_name} ({string_SQL_data});COPY {table_name} FROM '{path_csv}{table_name}.csv' DELIMITER ',' CSV HEADER ENCODING 'UTF8';\n", con, cur)
+
+
 def print_listing(listing : dict):
     for key in listing:
         print(key + " - " + str(listing[key]))
@@ -23,7 +151,6 @@ def is_float(num):
         return True
     except ValueError:
         return False
-
 
 def get_suggest_type(path_f, data_names):
     dict_suggest = {name : [] for name in data_names}
@@ -46,7 +173,7 @@ def get_suggest_type(path_f, data_names):
                 types["INT"] = False
             if types["BIGINT"] and (not elem.isnumeric() or abs(int(elem) > 2**63-1)):
                 types["BIGINT"] = False
-            if types["VARCHAR"] and int(size_varchar) < len(elem) :
+            if types["VARCHAR"] and int(size_varchar) < len(elem):
                 size_varchar = len(elem)
                 if size_varchar > 10485760:
                     types["VARCHAR"] = False
@@ -84,123 +211,94 @@ def waiting_input(msg : str, dict_data : dict, data_names : list, idx_column : i
         else:
             print("\nINVALID ENTRY\n")
 
+def execute_sql(query, con, cur):
+    try:
+        cur.execute(query)
+        con.commit()
+    except psycopg2.errors.DuplicateTable as e:
+        e = str(e)
+        res = input("\nDatabase already exist\nDrop the table ?\nY. Drop\nn. Cancel\n\n")
+        if res.upper() == "Y" or res == "":
+            con.rollback()
+            table_name = e.split("«")[1].split("»")[0] if "«" in e else e.split('"')[0]
+            cur.execute(f"DROP TABLE{table_name};")
+            con.commit()
+            cur.execute(query)
+            con.commit()
 
-your_database = input("Insert Database: ")
-your_user = input("Insert User: ")
-your_password = input("Insert Password: ")
-your_host = input("Insert Host: ")
-your_port = input("Insert Port: ")
+def export_sql(query, sql_file, option):
+    with open(sql_file, option, encoding="utf-8") as f:
+        f.write(query)
 
-# This script will iterate with all CSV files in a folder. DO NOT leave any non CSV files in your folder.
-path = "C:/YourPath"   #Insert Path of your folder
-
-for file in os.listdir(path):
-    if not file.endswith('.csv'):
-        continue
-    con = psycopg2.connect(database=your_database, user=your_user, password=your_password, host=your_host, port=your_port)   #To change connection settings manually, change them here
-    cur = con.cursor()
-    print(f"file : {file}")
-    filepath = path + file
-    f = open(filepath)
-    line = f.readline()
-
-    dict_data = {replace_invalid_characters(column) : [] for column in line.split(',')}
-    if len(dict_data) == 0:
-        print("header not found => next file")
-        continue
-
+def import_sql(path, con, cur):
+    files = os.listdir(path)
+    print(f"\nChoice the file to import ({len(files)+1}):")
+    for i in range(len(files)):
+        print(f"{i+1}. {files[i]}")
+    print(f"{len(files)+1}. Cancel\n")
+    res = input()
+    if not res.isnumeric() or int(res)<1 or int(res)>len(files):
+        return
+    else:
+        with open(f"{path}{files[int(res)-1]}", "r", encoding="utf-8") as f:
+            execute_sql(f.read(), con, cur)  
     
-    data_names = list(dict_data.keys())
+def drop_tables(con, cur):
+    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+    list_tables = []
+    for table in cur.fetchall():
+        list_tables.append(str(table).split("'")[1])
+    print(f"\nSelect the table ({len(list_tables)+2}):")
+    for i in range(len(list_tables)):
+        print(f"{i+1}. {list_tables[i]}")
+    if len(list_tables) >= 2:
+        print(f"{len(list_tables)+1}. All of them")
+    print(f"{len(list_tables)+2}. Cancel\n")
+    res = input()
+    selected_tables = []
+    if not res.isnumeric() or int(res)<1 or int(res)>len(list_tables)+1:
+        return
+    if len(list_tables) >= 2 and int(res) == len(list_tables)+1:
+        selected_tables = list_tables
+    else:
+        selected_tables.append(list_tables[int(res)-1])
+    for table in selected_tables:
+        cur.execute(f"DROP TABLE {table};")
+        con.commit()
+    
 
-    list_action = [[]]
-    types = {'1':"INT", '2':"BIGINT", '3':"VARCHAR", '4':"BOOL", '5':"SERIAL", '6':"DECIMAL"}
-    diff = ["VARCHAR"]
-    do_it_for_all = False
-    idx_column = 0
+
+def init_db():
+    print("If you wish to make it faster, change the entries directly on the code.")
+    your_database = input("Insert Database: ")
+    your_user = input("Insert User: ")
+    your_password = input("Insert Password: ")
+    your_host = input("Insert Host: ")
+    your_port = input("Insert Port: ")
+
+    return psycopg2.connect(database=your_database, user=your_user, password=your_password, host=your_host, port=your_port)   #To change connection settings manually, change them here
+
+def main(con):
+    # This script will iterate with all CSV files in a folder. DO NOT leave any non CSV files in your folder.
+    path_csv = "C:/YourPath"   #Insert Path of your csv folder
+    path_sql = "C:/YourPath"   #Insert Path of your sql folder
+
+    cur = con.cursor()
 
     while True:
-            print("\nList column :")
-            print_listing(dict_data)
-            print("\nCurrent column :\n"+data_names[idx_column])
-            res = input("\nMenu: (1) \n 1. next \n 2. do it for all \n 3. undo\n 4. suggest\n\n")
-            if res == '1' or res == '':
-                do_it_for_all = False
-            elif res == '2':
-                do_it_for_all = True
-            elif res == '3':
-                if(len(list_action) == 0):
-                    print("cannot execute undo")
-                    continue
-                for key in list_action[-1]:
-                    dict_data[key] = []
-                    idx_column -= 1
-                list_action.pop(-1)
-                continue
-            elif res == '4':
-                dict_suggest = get_suggest_type(filepath, data_names)
-                print("\nSuggest:")
-                for key in dict_suggest:
-                    msg = f"{key} : "
-                    for type in dict_suggest[key]:
-                        msg += f"{type} "
-                    print(f"{msg}")
-                continue
-            else:
-                continue
-
-            list_action.append([])
-            data_type = input("\nDataType? \n 1. INT \n 2. BIGINT \n 3. VARCHAR \n 4. BOOL \n 5. SERIAL \n 6. DECIMAL\n\n")
-            if data_type.isnumeric() and data_type in types:
-                dict_data[data_names[idx_column]].append(types[data_type])
-                if types[data_type] == "VARCHAR":
-                    waiting_input("Max size of string", dict_data, data_names, idx_column)
-                if types[data_type] == "DECIMAL":
-                    waiting_input("Total number size", dict_data, data_names, idx_column)
-                    waiting_input("Precision", dict_data, data_names, idx_column)
-                list_action[-1].append(data_names[idx_column])
-                idx_column += 1
-            else:
-                print('\nINVALID ENTRY\n')
-                continue
-            
-            if do_it_for_all:
-                idx_tmp = idx_column-1
-                while idx_column != len(data_names):
-                    dict_data[data_names[idx_column]] = dict_data[data_names[idx_tmp]]
-                    list_action[-1].append(data_names[idx_column])
-                    idx_column += 1
-                    
-            
-            if idx_column == len(data_names):
-                res = input("\nAll defined (1):\n1. Finish\n2. Undo\n\n")
-                if res == '2':
-                    for key in list_action[-1]:
-                        dict_data[key] = []
-                        idx_column -= 1
-                    list_action.pop(-1)
-                else:
-                    break;
-
-    SQL_data = []
-    for col in dict_data:
-        if dict_data[col][0] == "VARCHAR":
-            SQL_data.append(f"{col} {dict_data[col][0]}({dict_data[col][1]})")
-        elif dict_data[col][0] == "DECIMAL":
-            SQL_data.append(f"{col} {dict_data[col][0]}({dict_data[col][1]}, {dict_data[col][2]})")
+        res = input("\nMenu (1):\n1. Create table from csv files\n2. Import table from sql files\n3. Drop tables\n4. Exit\n\n")
+        if res == "2":
+            import_sql(path_sql, con, cur)
+        elif res == "3":
+            drop_tables(con,cur)
+        elif res == "4":
+            print("\nGoodbye !")
+            break
         else:
-            SQL_data.append(f"{col} {dict_data[col][0]}")
+            create_from_csv(path_sql, path_csv, con, cur)
 
-    #After the iteration, the script will build a PostgreSQL query based on the information given
-    string_SQL_data = ','.join(SQL_data)
-    query = sql.SQL("CREATE TABLE {some_table} ({table_data});").format(
-        some_table=sql.SQL(os.path.splitext(file)[0]),
-        table_data=sql.SQL(string_SQL_data))
-    cur.execute(query) 
-    string_SQL_data = []
-    con.commit()
-    query = sql.SQL("COPY {some_table} FROM '{original_file}' DELIMITER ',' CSV HEADER ENCODING 'UTF8';").format(
-        some_table=sql.SQL(os.path.splitext(file)[0]),
-        original_file=sql.SQL(filepath))
-    cur.execute(query) 
-    con.commit()
     con.close()
+
+if __name__ == "__main__":
+    con = init_db()
+    main(con)
