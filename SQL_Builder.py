3c2b9184f3adb41c1d29215c6f62dedc655efa10
AxolotlOfTruth <gabriel.lara.verite@gmail.com>
2021-04-12 10:39:24 +0200

    "Fixes",path input, adds some prints for the user

--- SQL_Builder.py
+++ SQL_Builder.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import sql, errors
 import os
 import csv
+from getpass import getpass
 
 def replace_invalid_characters(name):
     return ''.join(e for e in name if e.isalnum())
@@ -79,6 +80,7 @@ def create_from_csv(path_sql, path_csv, con, cur):
                         print(f"{msg}")
                     continue
                 else:
+                    print('\nINVALID ENTRY\n')
                     continue
 
                 list_action.append([])
@@ -105,6 +107,7 @@ def create_from_csv(path_sql, path_csv, con, cur):
                         
                 
                 if idx_column == len(data_names):
+                    print_listing(dict_data)
                     res = input("\nAll defined (1):\n1. Finish\n2. Undo\n\n")
                     if res == '2':
                         for key in list_action[-1]:
@@ -225,7 +228,16 @@ def execute_sql(query, con, cur):
             con.commit()
             cur.execute(query)
             con.commit()
-
+    except psycopg2.errors.SyntaxError as e:
+        e = str(e)
+        res = input("\nTable has invalid characters/protected words\nDrop the table ?\nY. Drop\nn. Cancel\n\n")
+        if res.upper() == "Y" or res == "":
+            con.rollback()
+            table_name = e.split("«")[1].split("»")[0] if "«" in e else e.split('"')[0]
+            cur.execute(f"DROP TABLE{table_name};")
+            con.commit()
+            cur.execute(query)
+            con.commit()
 def export_sql(query, sql_file, option):
     with open(sql_file, option, encoding="utf-8") as f:
         f.write(query)
@@ -272,7 +284,7 @@ def init_db():
     print("If you wish to make it faster, change the entries directly on the code.")
     your_database = input("Insert Database: ")
     your_user = input("Insert User: ")
-    your_password = input("Insert Password: ")
+    your_password = getpass("Insert Password: ")
     your_host = input("Insert Host: ")
     your_port = input("Insert Port: ")
 
@@ -280,8 +292,9 @@ def init_db():
 
 def main(con):
     # This script will iterate with all CSV files in a folder. DO NOT leave any non CSV files in your folder.
-    path_csv = "C:/YourPath"   #Insert Path of your csv folder
-    path_sql = "C:/YourPath"   #Insert Path of your sql folder
+    your_path = input("Insert Path: ")
+    path_csv = your_path   #Insert Path of your csv folder
+    path_sql = your_path   #Insert Path of your sql folder
 
     cur = con.cursor()
 
